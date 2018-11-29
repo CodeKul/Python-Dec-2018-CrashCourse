03b2fb955bba395f3bfee53c498c812172b4776c
Varun <varun.kudalkar@gmail.com>
2018-11-29 13:45:20 +0530

    first commit

---
+++ Variables.py
@@ -0,0 +1,54 @@
+# Hello World program in Python
+
+# single line comment
+"""
+
+This is
+Multiline 
+comment
+
+"""
+
+var = 10
+var = "Codekul"
+var = 'Codekul'
+
+var = '''"'Codekul'"
+    The "Gurukul" for Coders!'''
+
+print(var)
+
+x = 20
+
+print(x)
+
+a = 10
+b = 20
+d = 10.20
+
+c = a + b
+
+CONST = 100
+
+print(CONST)
+
+print(c)
+
+c = a + d
+print(c)
+
+e = False
+
+print(e)
+
+
+print(type(var))
+var = 10
+print(type(var))
+print(type(x))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
